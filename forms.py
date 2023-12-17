64e467b0a969b7e37bdfaede419cf74d00063b14
Sharia Parveen <109953777+Sharia12@users.noreply.github.com>
2023-12-17 19:54:38 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,23 +1,10 @@
+"""
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Item
 
-class NewUSerForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    phone_number = forms.CharField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
-
+class AddForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ("username", "email", "phone_number", "first_name", "last_name", "password1", "password2")
-
-    def save(self,commit=True):
-        user = super(NewUSerForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        user.phone_number = self.cleaned_data["phone_number"]
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        if commit:
-            user.save()
-        return user
+        model = Item
+        fields = ('created_by',
+        'title', 'image', 'description', 'price', 'pieces', 'instructions', 'labels', 'label_colour', 'slug')
+        """
